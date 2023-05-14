ad3ebdddd4ed529db4334380a420bba37f686aec
Lukasz Paczek <yourf4te@gmail.com>
2023-05-14 17:20:07 +0200

    updated .girignore

--- src/test/groovy/com/github/paczek9000/tests/TestDependencyPool.groovy
+++ src/test/groovy/com/github/paczek9000/tests/TestDependencyPool.groovy
@@ -6,19 +6,14 @@ import com.github.paczek9000.infrastructure.drivers.DriverManager
 import com.github.paczek9000.infrastructure.profileresolver.SystemActivePropertyProfileResolver
 import com.github.paczek9000.pages.DemoPage
 import com.github.paczek9000.utils.selenium.UrlResolver
-import org.junit.jupiter.api.extension.ExtendWith
 import org.openqa.selenium.WebDriver
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import spock.lang.Specification
 
-@ExtendWith(SpringExtension.class)
 @ActiveProfiles(value = ["dev", "chrome"], resolver = SystemActivePropertyProfileResolver.class)
 @SpringBootTest(classes = TestApplicationContext.class)
-@ContextConfiguration(classes = TestApplicationContext.class)
 class TestDependencyPool extends Specification {
 
     @Autowired
